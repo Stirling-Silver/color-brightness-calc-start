b16ba9ac9908d47a21288c24cdcfd6413f56f593
Stirling Luke <s.luke@EDU.epsb.ca>
2022-09-23 11:23:46 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("hi");
+// Brightness IPO calculator
+
+// Input
+let R = +prompt("Enter a R color value (Red):");
+let G = +prompt("Enter a G color value: (Green)");
+let B = +prompt("Enter a B color value: (Blue)");
+
+
+// Process
+let Brightness = Math.sqrt(0.299(R) ** 2 + 0.587(G) ** 2 + 0.114(B) ** 2);
+
+// Output
