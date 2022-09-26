921327e842bebeec32417f2fad774a3e8373a7bb
Stirling Luke <s.luke@EDU.epsb.ca>
2022-09-26 10:20:42 -0600

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ let B = +prompt("Enter a B color value: (Blue)");
 
 
 // Process
-let Brightness = Math.sqrt(0.299(R) ** 2 + 0.587(G) ** 2 + 0.114(B) ** 2);
+let Brightness = Math.sqrt(0.299 * (R) ** 2 + 0.587 * (G) ** 2 + 0.114 * (B) ** 2);
 
 // Output
+alert(`The brightness is: ${Brightness}`);
